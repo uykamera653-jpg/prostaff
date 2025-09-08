d142dc09c14d1a741b06a5ccf965767902d0838f
uykamera653-jpg <uykamera653@gmail.com>
2025-09-08 18:34:49 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,26 @@
-// Always-Fresh, cache-less SW
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
-  self.clients.claim();
+const CACHE = 'prostaff-v1';
+const ASSETS = [
+  './', './index.html', './bugun.html', './ertaga.html', './firms.html',
+  './style.css', './app.js', './icon-190.png', './icon-191.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(new Request(e.request, {cache: 'no-store'})).catch(() => caches.match(e.request))
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
+});
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin){
+    e.respondWith(
+      caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
+        const copy = res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request, copy));
+        return res;
+      }))
+    );
+  }
 });
