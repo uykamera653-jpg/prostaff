06b31ba2895e525cbdd0821fb83a7d5db6a88af9
uykamera653-jpg <uykamera653@gmail.com>
2025-09-08 12:52:29 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,35 @@
+const CACHE_NAME = "prostaff-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+// install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("prostaff-cache").then(cache => {
-      return cache.addAll(["/", "/index.html"]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
+// fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
+  );
+});
+
+// activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
   );
 });
