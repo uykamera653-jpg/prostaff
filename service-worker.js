7fdd8212a80d42f147c9df733cd4e0c709ae323a
uykamera653-jpg <uykamera653@gmail.com>
2025-09-08 13:03:25 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,29 @@
-const CACHE_NAME = "prostaff-cache-v1";
+const CACHE_NAME = "prostaff-cache-v2";
+const PREFIX = "./"; // /prostaff/ ostida ekanimiz uchun nisbiy yo'l
 const URLS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json"
+  `${PREFIX}`,
+  `${PREFIX}index.html`,
+  `${PREFIX}workers.html`,
+  `${PREFIX}firms.html`,
+  `${PREFIX}manifest.json`
 ];
 
-// install
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-// fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// activate
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
